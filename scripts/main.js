1393b751e92f59b114d947e3c3ee5ba222024aa4
Roxy Dee <roxydehart@gmail.com>
2017-11-25 14:58:34 -0600

    added set username button
    user is now prompted to enter their name and the heading will change to include the input. User can also change their name using the 'change user' button at the bottom of the page.

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,19 @@ myImage.onclick = function() {
       myImage.setAttribute ('src', 'pix/roxy_photo2.jpeg')
     }
 }
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Meet Roxy Dee, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+}   else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
